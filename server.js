37c88a82c73cb6be33d4d0cffa9b79ed25445b53
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-30 11:43:59 +0200

    completed getting employee data from mongodb

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ import { MongoClient } from "mongodb";
 const app = express();
 const port = 3022;
 
+const conn = "mongodb://localhost:27017";
+const client = new MongoClient(conn);
+
+await client.connect();
+const db = client.db("northwind");
+const employees = await db.collection("employees").find().toArray();
+console.log(employees);
+
 app.get("/", (req, res) => {
   res.send("<h1>MongoDB Test</h1>");
 });
